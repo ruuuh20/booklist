023c9507d6c8f1137d618b485f6cb1ab72fe4981
ruuuh20 <pkim2320@gmail.com>
2022-09-09 11:12:21 -0400

    revise/update prismicio page

--- prismicio.js
+++ prismicio.js
@@ -1,10 +1,53 @@
+// import * as prismic from '@prismicio/client'
+// import * as prismicH from '@prismicio/helpers'
+// import { enableAutoPreviews } from '@prismicio/next'
+// import sm from './sm.json'
+
+// export const endpoint = sm.apiEndpoint
+// export const repositoryName = prismic.getRepositoryName(endpoint)
+
+// // Update the Link Resolver to match your project's route structure
+// export function linkResolver(doc) {
+//   switch (doc.type) {
+//     case 'homepage':
+//       return '/'
+//     case 'page':
+//       return `/${doc.uid}`
+//     default:
+//       return null
+//   }
+// }
+
+// // This factory function allows smooth preview setup
+// export function createClient(config = {}) {
+//   const client = prismic.createClient(endpoint, {
+//     ...config,
+//   })
+
+//   enableAutoPreviews({
+//     client,
+//     previewData: config.previewData,
+//     req: config.req,
+//   })
+
+//   return client
+// }
+
 import * as prismic from '@prismicio/client'
-import { enableAutoPreviews } from '@prismicio/next'
+import * as prismicH from '@prismicio/helpers'
+import * as prismicNext from '@prismicio/next'
 import sm from './sm.json'
 
-export const endpoint = sm.apiEndpoint
-export const repositoryName = prismic.getRepositoryName(endpoint)
+/**
+ * The project's Prismic repository name.
+ */
+export const repositoryName = prismic.getRepositoryName(sm.apiEndpoint)
 
+/**
+ * The project's Prismic Link Resolver. This function determines the URL for a given Prismic document.
+ *
+ * @type {prismicH.LinkResolverFunction}
+ */
 // Update the Link Resolver to match your project's route structure
 export function linkResolver(doc) {
   switch (doc.type) {
@@ -17,13 +60,16 @@ export function linkResolver(doc) {
   }
 }
 
-// This factory function allows smooth preview setup
-export function createClient(config = {}) {
-  const client = prismic.createClient(endpoint, {
-    ...config,
-  })
+/**
+ * Creates a Prismic client for the project's repository. The client is used to
+ * query content from the Prismic API.
+ *
+ * @param config {prismicNext.CreateClientConfig} - Configuration for the Prismic client.
+ */
+export const createClient = (config = {}) => {
+  const client = prismic.createClient(sm.apiEndpoint, config)
 
-  enableAutoPreviews({
+  prismicNext.enableAutoPreviews({
     client,
     previewData: config.previewData,
     req: config.req,
